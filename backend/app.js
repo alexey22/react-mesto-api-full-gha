74b1733b31e312c163fe9ee4e976970c4882dc2f
Alexey Smirnov <alexey.smirnov.barnaul@yandex.ru>
2023-07-14 19:46:53 +0700

    first attempt test 3

--- backend/app.js
+++ backend/app.js
@@ -34,10 +34,33 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  family: 4,
-});
+const startServer = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/mestodb', {
+      useNewUrlParser: true,
+      family: 4,
+    });
+    // eslint-disable-next-line no-console
+    console.log('Успешно подключено к MongoDB');
+
+    try {
+      app.listen(PORT, () => {
+        // eslint-disable-next-line no-console
+        console.log('Ссылка на сервер');
+        // eslint-disable-next-line no-console
+        console.log(BASE_PATH);
+      });
+      // eslint-disable-next-line no-console
+      console.log(`Сервер успешно запущен на порте: ${PORT}`);
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.log('Ошибка запуска сервера', err);
+    }
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log('Ошибка подключения к MongoDB', err);
+  }
+};
 
 app.use(requestLogger);
 
@@ -55,9 +78,4 @@ app.use(errors()); // обработчик ошибок celebrate
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log('Ссылка на сервер');
-  // eslint-disable-next-line no-console
-  console.log(BASE_PATH);
-});
+startServer();
