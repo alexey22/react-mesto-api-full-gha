360d51969c33024d23717d2c3e8862f5ecb913e0
Alexey Smirnov <alexey.smirnov.barnaul@yandex.ru>
2023-07-14 19:51:40 +0700

    first attempt test 4

--- frontend/src/auth.js
+++ frontend/src/auth.js
@@ -1,6 +1,6 @@
 // export const BASE_URL = 'http://localhost:3000';
 // export const BASE_URL = 'https://auth.nomoreparties.co';
-export const BASE_URL = 'https://api.musli.nomoredomains.work/';
+export const BASE_URL = 'https://api.musli.nomoredomains.work';
 
 function handleResponse(res) {
   if (res.ok) {
